8a3d5447e38dda840c7e5d2b294cb11f49948750
YJ Soon <yjsoon@tinkertanker.com>
2021-08-19 12:28:28 +0800

    Updated for MAMP in starter code

--- starter code/dbconfig/config.php
+++ starter code/dbconfig/config.php
@@ -1,4 +1,6 @@
 <?php
 $con = mysqli_connect("localhost:3308", "root", "") or die("Unable to connect");
+// Use the line below if you're on MAMP
+//$con = mysqli_connect("localhost:8889", "root", "root") or die("Unable to connect"); 
 mysqli_select_db($con, "testdb");
 ?>
